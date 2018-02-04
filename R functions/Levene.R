a32ff491005afad9674745ffbf07f08776224c19
Unknown <tati.micheletti@gmail.com>
2018-02-04 15:19:26 -0800

    Working on Levene
    Still needs work.

--- R functions/Levene.R
+++ R functions/Levene.R
@@ -1,32 +1,40 @@
 # LEVENE FUNCTION FOR MULTIPLE GROUPS - PROJECT TAPIRUS
 
-# LEVENE FUNCTION FOR MULTIPLE GROUPS - PROJECT TAPIRUS
+levene.p <- function(dataset, firstCol, factor){
 
-levene.p <- function(dataset, firstCol){
+  # browser()
   
-  pant <- hembio.homog[hembio.homog$Biome=="Pantanal",] #Age was used for grouping: most restrictive comparison
-  pant.m <- hembio.homog[hembio.homog$Biome=="Pantanal"&hembio.homog$Sex=="Male",] #Age was used for grouping: most restrictive comparison
-  pant.f <- hembio.homog[hembio.homog$Biome=="Pantanal"&hembio.homog$Sex=="Female",]
-  cerr.m <- hembio.homog[hembio.homog$Biome=="Cerrado"&hembio.homog$Sex=="Male",]
-  cerr.f <- hembio.homog[hembio.homog$Biome=="Cerrado"&hembio.homog$Sex=="Female",]
-  
-  lv.test <- list()
-  lv.pvalue <- numeric()
+  lv.test <- numeric()
+  all.fac <- numeric()
   for (i in firstCol:ncol(dataset)){
-    if (sum(dataset[,i], na.rm=TRUE)>0){
-      lv.test$dataset[[i]] <- leveneTest(dataset[,i], group = dataset$Age) #Biome was used as the grouping factor for the test
-      lv.test$dataset[[i]] <- lv.test$dataset[[i]]$`Pr(>F)`[1]
-      Variables <- colnames(dataset[,firstCol:ncol(dataset)])
-      lv.pvalue[i] <- as.numeric(lv.test$dataset[[i]])
-    } else
-      lv.test$dataset[[i]] <- NA
+      vari.test <- data.table(FACTOR=dataset[,factor],VALUE=dataset[,i])
+      sum.fac <- vari.test[,.(VALUE.Sum=sum(!is.na(VALUE))),by=FACTOR]   
+  for (j in 1:length(levels(vari.test$FACTOR))){
+    all.fac[j] <- sum.fac[j]$VALUE.Sum
   }
-  lv.results <- data.frame(Variables, lv.pvalue)
+      all.fac <- unlist(all.fac)
+    if (all(all.fac>0)){
+      df <- data.frame(dataset[,i], dataset[,factor])
+      lv.test[i] <- leveneTest(df[,1], group=df[,2])$`Pr(>F)`[1]
+    } else
+      lv.test[i] <- NA
+    }
+  
+  Variables <- colnames(dataset[,firstCol:ncol(dataset)])
+  lv.results <- data.frame(Variables, Levene_p = lv.test[firstCol:length(lv.test)])
   lv.results$Homogeneity <- NA
   lv.results <- as.data.table(lv.results)
+
   for (i in 1:nrow(lv.results)){
-    if (lv.results$lv.pvalue[i]>0.05){
-      lv.results$Homogeneity[i] <- "HOMOGENEOUS" }
+  if (is.na(lv.results$Levene_p[i])){
+    lv.results$Homogeneity[i] <- "NON-HOMOGENEOUS" 
+  } 
+else
+    if (lv.results$Levene_p[i]>0.05){
+      lv.results$Homogeneity[i] <- "HOMOGENEOUS"      
+      }
+      else
+     lv.results$Homogeneity[i] <- "NON-HOMOGENEOUS"
   }
   
   dataset.levene <- lv.results
